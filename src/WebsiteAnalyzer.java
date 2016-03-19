6ee3fcebf5a581cc3b104f3fa44fa764fe707950
ankit.das <ankit.das@verse.in>
2016-03-19 14:50:31 +0530

    Both code snippets

---
+++ src/WebsiteAnalyzer.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class WebsiteAnalyzer {
+
+	// Maintaining a map of page url and access counts
+	private Map<String, Integer> pageMap = new HashMap<String, Integer>();
+
+	public void reportPageAccess(String pageUrl) {
+		// Since this operation will be called very frequently, just
+		// incrementing the page access count here, no extra computation
+		Integer count = pageMap.get(pageUrl);
+		if (count == null)
+			pageMap.put(pageUrl, 1);
+		else
+			pageMap.put(pageUrl, count + 1);
+	}
+
+	// This API should be synchronized
+	public List<String> getTopNPages(int n) {
+		// Since this operation will be called less frequently, doing all
+		// computation here
+		List<String> l = new ArrayList<>();
+
+		// Getting the values of the map and sorting in descending order.
+		List<Integer> valuelist = new ArrayList<Integer>(pageMap.values());
+		Comparator comparator = Collections.reverseOrder();
+		Collections.sort(valuelist, comparator);
+		//
+		for (Integer i : valuelist) {
+			// Iterating over the page map to find which keys have values equal
+			// to the max value of above list
+			for (Entry<String, Integer> entry : pageMap.entrySet()) {
+				if (entry.getValue().equals(i) && l.size() < n) {
+					l.add(entry.getKey());
+				}
+			}
+		}
+
+		return l;
+	}
+}
